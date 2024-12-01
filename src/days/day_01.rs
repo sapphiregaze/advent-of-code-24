456b7e5d3b6c1455e035b34df242e867c69d173f
SapphireGaze <jasminehuang2048@outlook.com>
2024-11-30 22:23:36 -0800

    Finished Day 1 Part 1

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -1,1 +1,27 @@
-pub fn run() {}
+use std::fs;
+
+pub fn run(file_path: &str) {
+    let contents = fs::read_to_string(file_path).expect("Failed to read file");
+    let location_ids: Vec<&str> = contents.split_whitespace().collect();
+
+    let mut location_list_one: Vec<u32> = Vec::new();
+    let mut location_list_two: Vec<u32> = Vec::new();
+
+    for (i, &location_id) in location_ids.iter().enumerate() {
+        if i % 2 == 0 {
+            location_list_one.push(location_id.parse().unwrap());
+        } else {
+            location_list_two.push(location_id.parse().unwrap());
+        }
+    }
+
+    location_list_one.sort();
+    location_list_two.sort();
+
+    let mut total_distance: u32 = 0;
+    for (i, &location_id) in location_list_one.iter().enumerate() {
+        total_distance += location_id.abs_diff(location_list_two[i]);
+    }
+
+    println!("{}", total_distance);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,19 @@ use std::env;
 mod days;
 
 fn main() {
-    let day = env::args().nth(1).and_then(|s| s.parse::<i8>().ok());
+    let args: Vec<String> = env::args().collect();
+
+    let (day, input_file) = parse_config(&args);
 
     match day {
-        Some(1) => days::day_01::run(),
-        Some(n) => println!("Day {} not implemented", n),
+        1 => days::day_01::run(input_file),
         _ => println!("invalid input."),
     }
+}
+
+fn parse_config(args: &[String]) -> (u8, &str) {
+    let day = args[1].parse().unwrap();
+    let input_file = &args[2];
+
+    (day, input_file)
 }
