6d8ac52d194842519f9b8a08b2bdc9a8d43182f4
SapphireGaze <jasminehuang2048@outlook.com>
2024-12-01 22:52:30 -0800

    Finished Day 2 Part 2

--- src/days/day_02.rs
+++ src/days/day_02.rs
@@ -20,11 +20,10 @@ pub fn run(file_path: &str) {
     let is_increasing = |report: &Vec<u8>| is_sorted_by(report, |x, y| x < y);
     let is_decreasing = |report: &Vec<u8>| is_sorted_by(report, |x, y| x > y);
     let is_in_range = |report: &Vec<u8>| -> bool {
-        let in_range = report
+        report
             .windows(2)
             .map(|s| (s[0], s[1]))
-            .all(|(x, y)| x.abs_diff(y) >= 1 && x.abs_diff(y) <= 3);
-        in_range
+            .all(|(x, y)| x.abs_diff(y) >= 1 && x.abs_diff(y) <= 3)
     };
 
     let is_safe = |report: &Vec<u8>| -> bool {
@@ -33,5 +32,34 @@ pub fn run(file_path: &str) {
 
     let num_safe_reports: usize = reports.iter().filter(|&report| is_safe(report)).count();
 
-    println!("Number of Safe Reports: {}", num_safe_reports)
+    println!("Number of Safe Reports: {}", num_safe_reports);
+
+    // Generates all possible permutations of report when a single level is removed
+    let report_permutations = |report: &Vec<u8>| -> Vec<Vec<u8>> {
+        let mut permutations: Vec<Vec<u8>> = Vec::new();
+        for i in 0..report.len() {
+            let mut modified = report.clone();
+            modified.remove(i);
+            permutations.push(modified);
+        }
+        permutations
+    };
+
+    // For each report, if any of the generated permutations is considered as
+    // safe, then that report is considered as safe
+    let mut num_safe_reports = 0;
+    for i in 0..reports.len() {
+        if (report_permutations(&reports[i]))
+            .iter()
+            .map(|report| is_safe(&report))
+            .any(|x| x)
+        {
+            num_safe_reports += 1;
+        }
+    }
+
+    println!(
+        "Number of Safe Reports (Problem Dampener): {}",
+        num_safe_reports
+    );
 }
