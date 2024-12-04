bd7c9ccee8bdf4460d804a940bb6f89d85f30698
SapphireGaze <jasminehuang2048@outlook.com>
2024-12-03 18:21:21 -0800

    Cleaned Up Day 3 Solution

--- src/days/day_03.rs
+++ src/days/day_03.rs
@@ -1,25 +1,25 @@
+use regex::{Captures, Regex};
 use std::fs;
-
-use regex::Regex;
 
 pub fn run(file_path: &str) {
     let contents = fs::read_to_string(file_path).expect("Failed to read file");
 
-    let re = Regex::new(r"(?m)mul\((?<num1>\d+),(?<num2>\d+)\)").unwrap();
+    let re = Regex::new(r"(?m)mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)").unwrap();
     let captures = re.captures_iter(&contents);
 
-    let factors = captures.map(|mat| {
+    let extract_and_parse = |capture: &Captures<'_>, i: usize| -> u32 {
+        capture
+            .get(i)
+            .expect("Invalid capture group")
+            .as_str()
+            .parse::<u32>()
+            .expect("Invalid number")
+    };
+
+    let factors = captures.map(|capture| {
         (
-            mat.get(1)
-                .expect("Invalid capture group")
-                .as_str()
-                .parse::<u32>()
-                .expect("Invalid number"),
-            mat.get(2)
-                .expect("Invalid capture group")
-                .as_str()
-                .parse::<u32>()
-                .expect("Invalid number"),
+            extract_and_parse(&capture, 1),
+            extract_and_parse(&capture, 2),
         )
     });
 
@@ -27,18 +27,25 @@ pub fn run(file_path: &str) {
 
     println!("Multiplication Results: {}", mul_results);
 
-    let re = Regex::new(r"(?m)(?<do>do\(\))|(?<dont>don't\(\))|mul\((?<num1>\d+),(?<num2>\d+)\)")
-        .unwrap();
+    let re = Regex::new(
+        r"(?m)(?<do>do\(\))|(?<dont>don't\(\))|mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)",
+    )
+    .unwrap();
     let captures = re.captures_iter(&contents);
 
     let captures_list = captures.fold(Vec::new(), |mut list, element| {
-        if let Some(value) = element.name("do").map(|m| m.as_str()) {
-            list.push(value);
-        } else if let Some(value) = element.name("dont").map(|m| m.as_str()) {
+        if let Some(value) = element
+            .name("do")
+            .or_else(|| element.name("dont"))
+            .map(|m| m.as_str())
+        {
             list.push(value);
         } else {
-            list.push(element.name("num1").map(|m| m.as_str()).unwrap());
-            list.push(element.name("num2").map(|m| m.as_str()).unwrap());
+            list.extend(
+                ["num1", "num2"]
+                    .iter()
+                    .filter_map(|&name| element.name(name).map(|m| m.as_str())),
+            );
         }
         list
     });
@@ -51,15 +58,15 @@ pub fn run(file_path: &str) {
                 "do()" => (acc, true, num, num_flag),
                 "don't()" => (acc, false, num, num_flag),
                 _ => {
-                    if mul_flag && !num_flag {
-                        let new_num = element.parse::<i32>().unwrap();
-                        (acc, mul_flag, new_num, true)
-                    } else if mul_flag && num_flag {
-                        let new_acc = acc + num * element.parse::<i32>().unwrap();
-                        (new_acc, mul_flag, 0, false)
-                    } else {
-                        (acc, mul_flag, num, num_flag)
+                    if !mul_flag {
+                        return (acc, mul_flag, num, num_flag);
                     }
+                    if !num_flag {
+                        let new_num = element.parse::<u32>().unwrap();
+                        return (acc, mul_flag, new_num, true);
+                    }
+                    let new_acc = acc + num * element.parse::<u32>().unwrap();
+                    (new_acc, mul_flag, 0, false)
                 }
             },
         )
